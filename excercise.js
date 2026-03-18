c5a3cb547bec5fe3b840164ae5efed418d729662
Kenny Portes <porteskenny@gmail.com>
2026-03-17 23:41:51 -0400

    Completed Task 1

---
+++ excercise.js
@@ -0,0 +1,17 @@
+// Task 1:
+let students = ['Kenny', 'Jackie','Aliyah','Catherine','Maria'];
+
+console.log("Original student list:");
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
+students.push('Joseph');
+console.log(students);
+students.pop();
+console.log(students);
+
+
+console.log("Original student list:");
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
