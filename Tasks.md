a3b48097ee635aefe4512a05ffdf467fc8263023
Kenny Portes <porteskenny@gmail.com>
2026-03-18 00:55:05 -0400

    compeled task2

---
+++ Tasks.md
@@ -0,0 +1,33 @@
+Data Structures Coding Task 
+
+Task 1 – Student List (Array)
+
+Create an array called students that stores 5 student names.
+Then perform the following operations: Use Data structures and algorithm theory module 2 to solve this problem.
+
+1.	Print all student names using a loop
+2.	Add a new student to the list
+3.	Remove the last student from the list
+4.	Print the updated list
+
+
+Task 2 – Number Operations
+
+Create an array with the numbers:
+[10, 20, 30, 40, 50]
+
+Students must:
+1.	Print all numbers using a loop
+2.	Find the sum of all numbers
+3.	Find the largest number
+
+
+Task 3 – Search in Array
+
+Create an array of fruits:
+["Apple", "Banana", "Mango", "Orange"]
+
+Ask students to:
+1.	Check if "Mango" exists in the array
+2.	Print "Found" if it exists
+3.	Print "Not Found" if it does not
--- excercise.js
+++ excercise.js
@@ -1,24 +1,62 @@
 // Task 1:
+
+// 1.)
+
 let students = ['Kenny', 'Jackie','Aliyah','Catherine','Maria'];
 
 console.log("Original student list:");
-for (let i = 0; i < students.length; i++) {
+for (let i=0; i<students.length; i++){
     console.log(students[i]);
 }
+
+// 2.)
+console.log('Added a New Student to the list:');
 students.push('Joseph');
 console.log(students);
+
+// 3.)
+console.log('Removed the last student from the list');
 students.pop();
 console.log(students);
 
+// 4.)
+console.log("Updated Student list:");
+students.forEach(student => console.log(student));
 
-console.log("Original student list:");
-for (let i = 0; i < students.length; i++) {
-    console.log(students[i]);
-}
-
-
-console.log('------------------------');
+console.log('-------------------------------------------');
 
 // ------------------------------------------------------------------------------------------------------
 
 // Task 2
+// 1.)
+
+let numbers = [10, 20, 30, 40, 50];
+
+console.log('Number list:')
+for (let num of numbers) {
+    console.log(num);
+}
+
+// 2.)
+
+console.log('Sum of all numbers:');
+let sum = 0;
+
+numbers.forEach(num => {
+    sum = sum + num;
+})
+console.log(sum);
+
+// 3.)
+
+console.log("Largest Number:");
+
+let largest = numbers[0];
+for (let num of numbers) {
+    if (num > largest) {
+        largest = num;
+    }
+}
+console.log(largest);
+
+console.log('-------------------------------------------');
